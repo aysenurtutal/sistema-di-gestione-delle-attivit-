e86429310146fe8761d6b3bef11e97737c765a1c
aysenur.ozdemir <aysenur.ozdemir@caretta.net>
2024-09-03 14:44:54 +0300

    project based created

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = { // Loreal ortamı
+  production: true,
+  uat: false,
+  development: false,
+  baseRoot: 'https://harmonyapi.loreal.com.tr/',
+  apiRoot: 'https://harmonyapi.loreal.com.tr/api/',
+  domain: 'https://harmony.loreal.com.tr/',
+  paymentPortalDomain: 'https://paymentportal.loreal.com.tr/',
+  defaultImage: './assets/img/default.png',
+};
---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  uat: true,
+  development: false,
+  baseRoot: 'https://harmonyapi.loreal.com.tr/',
+  apiRoot: 'https://uat-harmonyapi.loreal.com.tr/api/',
+  domain: 'https://uat-harmony.loreal.com.tr/',
+  paymentPortalDomain: 'https://paymentportal-stg.loreal.com.tr/',
+  defaultImage: './assets/img/default.png',
+};
